8597947958b3cd15cfb29a4bdb3a3d9b59a08976
chromiumsilver <jiaying_yao@berkeley.edu>
2023-10-17 22:53:16 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+  if (
+    !Number.isInteger(numOne) ||
+    !Number.isInteger(numTwo) ||
+    numOne < 0 ||
+    numTwo < 0
+  ) {
+    return "ERROR";
+  }
+  let startInt = numOne,
+    endInt = numTwo;
+  if (numOne > numTwo) {
+    startInt = numTwo;
+    endInt = numOne;
+  }
+  let sum = 0;
+  for (let num = startInt; num <= endInt; num++) {
+    sum += num;
+  }
+  return sum;
 };
 
 // Do not edit below this line
