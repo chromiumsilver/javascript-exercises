15053bc6e7ddbe7f04ca5863784c327e61542bf5
chromiumsilver <jiaying_yao@berkeley.edu>
2023-10-17 22:26:09 +0800

    04_removeFromArray version 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...values) {
+  for (let value of values) {
+    const idx = arr.indexOf(value);
+    if (idx != -1) {
+      arr.splice(idx, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
