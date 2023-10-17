b81e486a0337e79d66c7759ea188cf630a4c914e
chromiumsilver <jiaying_yao@berkeley.edu>
2023-10-17 21:16:43 +0800

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+  if (times < 0) return "ERROR";
 
+  let resultString = "";
+  for (let i = 0; i < times; i++) {
+    resultString += str;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
