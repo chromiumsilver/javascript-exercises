6a56489d577d8082c8d0d7f319e2be035c94422d
chromiumsilver <jiaying_yao@berkeley.edu>
2023-10-18 11:59:54 +0800

    Finish 09_panlindromes, using Regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return cleanStr.split("").reverse().join("") === cleanStr;
 };
 
 // Do not edit below this line
