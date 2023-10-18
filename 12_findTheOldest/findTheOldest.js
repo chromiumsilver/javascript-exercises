8eb96fc4f7aa141f1cb6d2bd5c1ac98a681d9a3c
chromiumsilver <jiaying_yao@berkeley.edu>
2023-10-18 16:11:16 +0800

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge > currentAge ? oldest : current;
+  });
+};
 
+const getAge = function (birth, death) {
+  return (death || new Date().getFullYear()) - birth;
 };
 
 // Do not edit below this line
