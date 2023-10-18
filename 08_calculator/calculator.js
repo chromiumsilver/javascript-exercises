9b3bb3f935feff448fcb184d94255f53313f2ed8
chromiumsilver <jiaying_yao@berkeley.edu>
2023-10-18 11:45:18 +0800

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, x) => sum + x, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prod, x) => prod * x, 1);
 };
 
-const power = function() {
-	
+const power = function (x, n) {
+  return x ** n;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let prod = 1;
+  for (let x = n; x >= 1; x--) {
+    prod *= x;
+  }
+  return prod;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
